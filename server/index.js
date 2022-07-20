3b6a1fe6cd75021d804c6a1dd9dd28837d2bc21d
pedrofzero <pedrolf2000@gmail.com>
2022-07-20 13:24:56 +0100

    fix gitignore

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const stripe = require('stripe')('sk_test_QUXcoU3BnbZXp6IMVi7BkW8s');
 const con = require('./db/db');
 const app = express();
 
@@ -10,23 +9,6 @@ app.use(cors());
 
 app.get('/', (req, res) => {
     res.send(con)
-})
-
-app.post('/create-checkout-session', async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: '{{PRICE_ID}}',
-            quantity: 1,
-          },
-        ],
-        mode: 'payment',
-        success_url: `${YOUR_DOMAIN}?success=true`,
-        cancel_url: `${YOUR_DOMAIN}?canceled=true`,
-      });
-    
-      res.redirect(303, session.url);
 })
 
 app.get('/getShirts', (req, res) => {
