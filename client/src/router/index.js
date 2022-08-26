8aba35ddde3955a527292599babf933646502201
pedroofzero <pedrolf2000@gmail.com>
2022-08-26 00:21:27 +0000

    fix router cuz login/reg

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,11 +2,9 @@ import React, { Suspense } from 'react'
 import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom'
 
 import Home from '../components/home'
-import Login from '../components/login'
 import Products from '../components/products'
 import AppLayout from '../layout/appLayout'
 import SingleProduct from '../components/product'
-import Register from '../components/register'
 
 const Router = () => {
     return (
@@ -18,12 +16,12 @@ const Router = () => {
                         <Route exact path="/home">
                             <Route index element={<Home />} />
                         </Route>
-                        <Route exact path="/login">
+                        {/* <Route exact path="/login">
                             <Route index element={<Login />} />
                         </Route>
                         <Route exact path="/register">
                             <Route index element={<Register />} />
-                        </Route>
+                        </Route> */}
                         <Route exact path="/product/:id">
                             <Route index element={<SingleProduct />} />
                         </Route>
